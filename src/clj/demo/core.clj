b51741527a53ae7c7d6caddcf067cf03c0a9fbc1
Alan Thompson <cloojure@gmail.com>
2020-02-11 01:10:06 -0800

    Consolidated current-source-ns#, working in tst.demo.core

--- src/clj/demo/core.clj
+++ src/clj/demo/core.clj
@@ -7,15 +7,18 @@
   [exception]
   (into [] (.getStackTrace exception)))
 
-(defmacro current-source-ns
-  "Returns a string containing the namespace where this macro is called"
-  [] '(try
-        (throw (ex-info "throwing" {}))
-        (catch Exception ex
-          (it-> ex
-            (exception-stacktrace-elems it)
-            (first it)
-            (.getClassName it)
-            (re-find #"[.\w]+" it)))))
-
+;**************************************
+;*****  SAVE SAVE SAVE SAVE SAVE  *****
+;**************************************
+;(defmacro current-source-ns
+;  "Returns a string containing the namespace where this macro is called"
+;  [] '(try
+;        (throw (ex-info "throwing" {}))
+;        (catch Exception ex
+;          (it-> ex
+;            (exception-stacktrace-elems it)
+;            (first it)
+;            (.getClassName it)
+;            (re-find #"[.\w]+" it)))))
+;
 
--- test/clj/tst/demo/core.clj
+++ test/clj/tst/demo/core.clj
@@ -27,46 +27,61 @@
 ;---------------------------------------------------------------------------------------------------
 
 
-(dotest
-  (is= "tst.demo.core" (spyx (current-source-ns)))
-  )
+;(dotest
+;  (is= "tst.demo.core" (spyx (current-source-ns)))
+;  )
 
+; #todo pull in this entire code block as a literal, then use eval in THIS ns!
 (defn macro-show-impl
   [macro-expr]
-  (let [macro-sym       (first macro-expr)
-        macro-sym-fq    `~macro-sym
-        macro-impl-sym  (str->sym (str (current-source-ns) "/"
-                                    (sym->str macro-sym) "-impl"))
-        macro-args      (rest macro-expr)
-        macro-impl-call (list macro-impl-sym `(quote ~(vec macro-args)))
-        macro-result    (eval macro-impl-call)]
-    (spyx macro-impl-sym)
-    (spyx macro-sym-fq)
-    (spyx macro-impl-call)
-    (spyx macro-result)
-    macro-result))
+ `(do
+   ;(spyxx (quote ~macro-expr))
+    (let [macro-sym#         (first (quote ~macro-expr))
+          macro-args#        (rest (quote ~macro-expr))
+
+          current-source-ns# (try
+                               (throw (Exception. "dummy throw"))
+                               (catch Exception ex#
+                                 (->> ex#
+                                   (exception-stacktrace-elems)
+                                   (first)
+                                   (.getClassName)
+                                   (re-find #"[.\w]+"))))
+        ; xxx#               (println :50 current-source-ns#)
+
+          macro-impl-sym#    (str->sym (str current-source-ns# "/"
+                                         (sym->str macro-sym#) "-impl"))
+          macro-impl-call#   (list macro-impl-sym# `(quote ~(vec macro-args#)))
+          macro-result#      (->list (eval macro-impl-call#))
+          ]
+      ;(spyx macro-impl-sym#)
+      ;(spyx macro-impl-call#)
+      ;(spyxx macro-result#)
+      (pretty macro-result#)
+      )))
 
 (defmacro macro-show
   [macro-expr]
-  `(pprint/pprint
-    (quote ~(macro-show-impl macro-expr))))
-
+  (macro-show-impl macro-expr))
 
 (dotest
+
   (nl) (println :macro-show-impl-output-begin)
   (pretty (macro-show-impl '(unless false (println "forms!") :yes)))
   (println :macro-show-impl-output-done)
-  (nl)
-  (println :macro-show-begin)
+
+  (nl) (println :macro-show-begin)
   (macro-show
     (unless false (println "forms!") :yeeessssss!))
   (println :macro-show-done)
 
-  (nl)
-  (println "calling...")
-  (is (unless false
-        (println "forms!") :yeeessssss!))
-  (println "  ...done")
+
+  ;(nl)
+  ;(println "calling...")
+  ;(is (unless false
+  ;      (println "forms!") :yeeessssss!))
+  ;(println "  ...done")
+
   )
 
 
